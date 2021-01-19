bdb12082c4a851d5ccc045bd9fd3955070ac6424
Eshwar S M <75710722+eshwarsm@users.noreply.github.com>
2021-01-19 19:34:44 +0530

    Update google_meet_automation.py

--- google_meet_automation.py
+++ google_meet_automation.py
@@ -1,12 +1,3 @@
-'''
-#1 >> Mention your Chrome Driver path
-#2 >> Change the username
-#3 >> Change the password
-#4 >> Mention your Google Meet Links
-#5 >> Schedule links as per your timetable
-#6 >> Enjoy!!!
-'''
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import schedule
